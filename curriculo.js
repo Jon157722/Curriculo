2396b9bfc3db393e4e9a39feec8c9689a9953c09
site <jonathan.santos.97@hotmail.com>
2020-03-15 17:21:56 -0300

    arquivos atualizados

---
+++ curriculo.js
@@ -0,0 +1,27 @@
+// var loadImages;
+
+// $(document).ready(function(){
+//     $(".lazy-img foto").on(load,function(){
+//         $(this).addClass("loaded");
+
+//     });
+
+//     (loadImages = function(){
+//         $.each(S(".lazy-img"), function(){
+//             var block = $(this);
+//             var image = block.find("foto");
+
+//             if(isOnScreen(block)){
+//                 var url = img.data("url");
+
+//                 if(image.attr("src") != url){
+//                     image.attr("src", url);
+//                 };
+//             }
+
+//         } );
+
+
+//     });
+
+// });
